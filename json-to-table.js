955aa532586a0cdbcfa2dafa13fbdd90e9149fd6
lachok <lacho.k@gmail.com>
2011-11-23 01:39:31 +0000

    Added support for a configuration object for the keys parameter. Array is still supported too.
    This object is in the following format:
    {
      header1_key: "Header 1",
      header2_key: "Header 2",
      header3_key: {
        name: "Header 3",
        onRender: function(input) {
                    alert('rendering a cell in column 3. value = ' + input);
                    return input;
                  }
      }
    }

--- json-to-table.js
+++ json-to-table.js
@@ -14,9 +14,20 @@ String.prototype.format = function() {
 };
 
 /**
+ * Check if an object is an array
+ * 
+ * @param input object to test
+ *
+ * @return boolean true or false
+ */
+function isArray(input){
+    return typeof(input)=='object'&&(input instanceof Array);
+}
+
+/**
  * Convert json to html table
  * 
- * @author Afshin Mehrabani <afshin dot meh at gmail dot com>
+ * @author Lacho Kozhuharov github.com/lachok, based on work by Afshin Mehrabani <afshin dot meh at gmail dot com>
  * 
  * @param jsonData string Json data
  * @param keys array Keys for table header
@@ -30,29 +41,60 @@ function ConvertJsonToTable(jsonData, keys, containerId, tableClassName) {
     var tbl = "<table border='1' cellpadding='1' cellspacing='1' id='" + containerId + "' class='" + tableClassName + "'>{0}{1}</table>";
     var th = "<thead>{0}</thead>";
     var tb = "<tbody>{0}</tbody>";
-    var tr = "<tr>{0}</tr>";
+    var tr = '<tr class="{0}">{1}</tr>';
     var thRow = "<th>{0}</th>";
-    var tdRow = "<td>{0}</td>";
+    var tdRow = '<td>{0}</td>';
     var thCon = "";
     var tbCon = "";
     var trCon = "";
 
+    function getRowClass(isOdd) {
+        return isOdd ? 'odd' : 'even';
+    }
+
     if (keys && jsonData) {
 
         //Creating all table headers
-        for (i = 0; i < keys.length; i++) {
-            thCon += thRow.format(eval(keys[i]));
+        if(isArray(keys)) {
+            for (i = 0; i < keys.length; i++) {
+                thCon += thRow.format(keys[i]);
+            }
+        } else {
+            for(var key in keys) {
+                if(keys.hasOwnProperty(key)) {
+                    if(keys[key].hasOwnProperty('name')) {
+                        thCon += thRow.format(keys[key].name);
+                    } else {
+                        thCon += thRow.format(keys[key]);
+                    }
+                }
+            }
         }
-        th = th.format(tr.format(thCon));
+
+        th = th.format(tr.format('header', thCon));
 
         //Creating all table rows from Json data
         if (typeof(jsonData[0]) == "object") {
+            var isOdd = false;
             for (i = 0; i < jsonData.length; i++) {
-                for (j = 0; j < keys.length; j++) {
-                    tbCon += tdRow.format(jsonData[i][keys[j]]);
+                if(isArray(keys)) {
+                    for (j = 0; j < keys.length; j++) {
+                        tbCon += tdRow.format(jsonData[i][keys[j]]);
+                    }
+                } else {
+                    for(var key in keys) {
+                        if(keys.hasOwnProperty(key)) {
+                            if(keys[key].hasOwnProperty('onRender')) {
+                                tbCon += tdRow.format(keys[key].onRender(jsonData[i][key]));
+                            } else {
+                                tbCon += tdRow.format(jsonData[i][key]);
+                            }
+                        }
+                    }
                 }
-                trCon += tr.format(tbCon);
+                trCon += tr.format(getRowClass(isOdd), tbCon);
                 tbCon = "";
+                isOdd = !isOdd;
             }
         }
         tb = tb.format(trCon);
